7ff9793d3b11c36fc2dd23eeff60a901f874f5e6
peibozhao <peibozhao@163.com>
2021-05-17 22:59:28 +0800

    Init

---
+++ dash_recorder/_imports_.py
@@ -0,0 +1,5 @@
+from .DashRecorder import DashRecorder
+
+__all__ = [
+    "DashRecorder"
+]
---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import DashRecorder from './components/DashRecorder.react';
+
+export {
+    DashRecorder
+};
